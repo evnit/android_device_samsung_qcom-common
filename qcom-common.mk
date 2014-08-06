d2a10fc92927452ed831186e5c34dce0e932c0c2
evnit <evnithn@gmail.com>
2014-08-06 21:21:56 +0700

    Update android_device_samsung_qcom-common

--- qcom-common.mk
+++ qcom-common.mk
@@ -12,28 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    device/samsung/qcom-common/overlay
 
-# System properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    com.qc.hardware=true
+# Media config
+PRODUCT_PACKAGES += media_codecs.xml
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.egl.hw=1 \
-    debug.sf.hw=1 \
-    debug.mdpcomp.logs=0 \
-    persist.hwc.mdpcomp.enable=true
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.call_ring.multiple=0 \
-    ro.telephony.ril_class=SamsungQualcommRIL
-
-# Charger
+# QCOM Display
 PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.egl.hw=1 \
+    persist.hwc.mdpcomp.enable=true \
+    debug.mdpcomp.logs=0 \
+    ro.telephony.ril_class=SamsungQualcommRIL \
+    ro.telephony.call_ring.multiple=0
+
+# Omx
+PRODUCT_PACKAGES += \
+    libdivxdrmdecrypt \
+    libmm-omxcore \
+    libOmxCore \
+    libc2dcolorconvert \
+    libOmxVdecHevc \
+    libstagefrighthw \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libdashplayer
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
@@ -41,9 +53,39 @@ PRODUCT_PACKAGES += \
     e2fsck \
     setup_fs
 
-# Recovery
+# Thermal profiles
 PRODUCT_PACKAGES += \
-    init.recovery.qcom.rc
+    thermald-8064ab.conf \
+    thermald-8064.conf \
+    thermald-8930.conf \
+    thermald-8930ab.conf \
+    thermald-8960.conf \
+    thermald-8260a.conf \
+    thermald-8960ab.conf \
+    thermal-engine-8064ab.conf \
+    init.qcom.thermal_conf.sh
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+# Init scripts
+PRODUCT_PACKAGES += \
+    init.qcom.post_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.sh \
+    init.qcom.class_core.sh \
+    init.qcom.class_main.sh \
+    init.qcom.syspart_fixup.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.mdm_links.sh \
+    init.qcom.modem_links.sh \
+    init.qcom.usb.sh \
+    lpm.rc \
+    init.qcom.lpm_boot.sh \
+    init.recovery.qcom.rc \
+    init.qcom.ril.sh
+
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# Common overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/qcom-common/overlay
